26e5292f5c36861e9cd6d49bc0c251bd85552340
Loucif Tameur Ahmed <tameur_ahmed.loucif@univ-constantine2.dz>
2026-03-11 01:58:19 +0100

    problem

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,7 +7,11 @@ dotenv.config();
 connectdatabase();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
 app.use(express.json());
 app.use("/note", noterouter);
 app.listen(5000, () => {
